8a9070893c2b16d4ef2aff3ed40dbc2f43ea8f76
heiheidodo <heiheidodo@gmail.com>
2016-04-22 15:38:35 -0400

    Update Joe

---
+++ BruceLeeSavesLucy/Joe.java
@@ -0,0 +1,42 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Joe here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Joe extends Figure
+{
+    int current_motion_index = 0;
+    int moveSpeed = 3;
+    int moveVariable = 0;
+    int moveStepLength = 10;
+    
+    public Joe()
+    {
+        stand2LeftSet = new GreenfootImage[12];
+        for(int i=0; i<stand2LeftSet.length;i++)
+        {
+            int m = i+ 1;
+            if (m < 10)
+            {
+                stand2LeftSet[i] = new GreenfootImage("Joe98_stance1_f0" + m +".gif");
+            }
+            else
+            {
+                stand2LeftSet[i] = new GreenfootImage("Joe98_stance1_f" + m +".gif");
+            } 
+        }
+        this.currentMotionSet = stand2RightSet;
+        this.setCurrentPose(Figure.POSE_STAND);
+    }
+    /**
+     * Act - do whatever the Joe wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
