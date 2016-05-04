3dc32f748b4af8b3675a907b2f8f4f7a445611f8
Martin <tmartin@126.com>
2016-05-04 12:29:39 -0700

    adding observer pattern

---
+++ BruceLeeSavesLucy/IObserver.java
@@ -0,0 +1,10 @@
+/**
+ * Write a description of class IObserver here.
+ * 
+ * @author (Ming Tang) 
+ * @version (a version number or a date)
+ */
+public interface IObserver  
+{
+    public void update(Figure f);
+}
---
+++ BruceLeeSavesLucy/ISubject.java
@@ -0,0 +1,12 @@
+/**
+ * Write a description of class ISubject here.
+ * 
+ * @author (Ming Tang) 
+ * @version (a version number or a date)
+ */
+public interface ISubject  
+{
+    public void attachObserver(IObserver ob);
+    public void detachObserver();
+    public void notifyObserver();
+}
