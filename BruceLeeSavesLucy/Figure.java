f7b75ab00b9112e80f77183efdecdecf8b44ed51
Martin <tmartin@126.com>
2016-05-05 14:00:05 -0700

    as

--- BruceLeeSavesLucy/Figure.java
+++ BruceLeeSavesLucy/Figure.java
@@ -2,22 +2,26 @@
 import java.util.*;
 import greenfoot.*;
 import java.awt.Color;
+import java.util.List;
 
 /**
  * 
  */
-public class Figure extends Actor implements ISubject
+public abstract class Figure extends Actor implements ISubject
 {
-    /* the health point*/
+    /* the current health point*/
     private int hp = 0;
+    private int initHp = 100;
     private String name = "name me :(";
     /* the denfence ability indicates the point that can be deducted from every attack*/
-    final static int walkingSpeed = 5;
+    private int walkingSpeed = 5;
     /* ever attack a figure gets should be deducted by this attribute */
-    final static int basicDenfence = 10;
-    private int currentPose = 0;
+    private int basicDenfence = 10;
+    
     public final static int POSE_STAND = 0, POSE_WALK = 1, POSE_PUNCH = 2, POSE_KICK=3;
     public final static int DIRECTION_RIGHT = 0, DIRECTION_LEFT = 1;
+    private IFighter targetFighter;
+    private int currentPose = POSE_STAND, figureRadius = 50;
     public GreenfootImage[] stand2RightSet,
     stand2LeftSet,
     walk2LeftSet,
@@ -26,7 +30,7 @@ public class Figure extends Actor implements ISubject
     punch2RightSet,
     currentMotionSet;
     IObserver observer;
-    public int direction = 0;
+    public int direction = DIRECTION_RIGHT ;
     /**
      * 
      */
@@ -43,6 +47,26 @@ public class Figure extends Actor implements ISubject
     public void stand(){
     
     }
+    public boolean isRightMovable(){
+        List<Figure> fs = getObjectsInRange(figureRadius,Figure.class);
+        if(fs.size()==0)
+            return true;
+        if (fs.get(0).getX() >= this.getX()){
+            targetFighter = (IFighter)fs.get(0);
+            return false;
+        }
+        return true;
+    };
+    public boolean isLeftMovable(){
+        List<Figure> fs = getObjectsInRange(figureRadius,Figure.class);
+        if(fs.size()==0)
+            return true;
+        if (fs.get(0).getX() <= this.getX()){
+            targetFighter = (IFighter)fs.get(0);
+            return false;
+        }
+        return true;
+    };
     public int getCurrentPose(){
         return this.currentPose;
     }
@@ -69,7 +93,7 @@ public class Figure extends Actor implements ISubject
     }
     public void setPoseWalkLeft(){
     }
-        public void addKeyCommandReceiverSuccessor(IKeyCommandReceiver rec){
+    public void addKeyCommandReceiverSuccessor(IKeyCommandReceiver rec){
     }
     public void removeKeyCommandReceiverSuccessor(){
     }
@@ -84,5 +108,35 @@ public class Figure extends Actor implements ISubject
     }
     public String getName(){
         return this.name;
+    }
+    public void setName(String n){
+        this.name = n;
+    }
+    public IFighter getTargetFighter(){
+        return this.targetFighter;
+    }
+    public void setTargetFighter(IFighter f){
+        this.targetFighter = f;
+    }
+    public int getCurrentHP(){
+        return this.hp;
+    }
+    public void setCurrentHP(int h){
+        this.hp = h;
+    }
+    public int getInitHP(){
+        return this.initHp;
+    }
+    public void setInitHP(int h){
+        this.initHp = h;
+    }
+    public void setDefencePoint(int d){
+        this.basicDenfence = d;
+    }
+    public int getDefencePoint(){
+        return this.basicDenfence;
+    }
+    public void setFigureRadius(int r){
+        this.figureRadius = r;
     }
 }
