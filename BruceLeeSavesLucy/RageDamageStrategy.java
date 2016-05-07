118f7af79f6f4f9d18e38f4cb1f5263a30e5ec21
archerlmlzmg <mliu82@wisc.edu>
2016-05-06 23:17:19 -0700

    add strategy for Bruce Lee

---
+++ BruceLeeSavesLucy/RageDamageStrategy.java
@@ -0,0 +1,11 @@
+public class RageDamageStrategy implements Strategy {
+
+	@Override
+	public int algorithm() {
+		// TODO Auto-generated method stub
+		System.out.println("NormalDamageStrategy");
+		return 50;
+		
+	}
+
+}
